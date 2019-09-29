cefca03f0263d7d5d66e328280c5fe6a2dc9343d
Sean Fyffe <stfyffe@gmail.com>
2019-09-29 16:15:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,6 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
-  add_element_to_end_of_array (array, element)
 end
 
 def add_element_to_start_of_array(array, element)
