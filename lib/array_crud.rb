caa9b2f237f8555d6bb47efcb64041a5087783dd
Sean Fyffe <stfyffe@gmail.com>
2019-09-29 16:52:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,8 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  element 0 = "wow"
+  
 end
 
 def retrieve_last_element_from_array(array)
